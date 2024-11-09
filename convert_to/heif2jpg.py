c0751c2a0bb749d6c281c293fd4fec7004937e97
Suprim Nakarmi <suprimnakarmi@gmail.com>
2024-11-09 16:48:30 -0600

    Added comments

--- convert_to/heif2jpg.py
+++ convert_to/heif2jpg.py
@@ -1,3 +1,10 @@
+"""
+This python script reads the HEIC images (Iphone pictures) and converts it into jpeg format for easier usages in the machine learning frameworks. 
+Owner: Suprim Nakarmi
+Date created: April, 2024
+
+"""
+
 from PIL import Image
 from pillow_heif import register_heif_opener
 import time
@@ -12,6 +19,10 @@ start_time = time.perf_counter()
 register_heif_opener()
 
 def heif_to_jpeg(file):
+    '''
+    Function to read file and convert it to jpeg format
+    file: Complete path of the file
+    '''
     image_name = file.split("/")[-1]
     index_dot = image_name.find(".")
     file_name = image_name[:index_dot]
@@ -32,3 +43,14 @@ with ThreadPoolExecutor(max_workers=5) as executor:
 end_time = time.perf_counter()
 time_taken = end_time - start_time
 print(f"Time taken for conversion: {time_taken}")
+
+
+'''
+Experimentations:
+Image size: 85 
+Execution time (without multithreading): 24.43 seconds
+Execution time (with multithreading): 10.23 seconds
+
+Concurrent multithreading was used for its automatic allocation of threads to process. 
+
+'''
