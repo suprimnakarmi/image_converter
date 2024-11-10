53e0b7f4a53f99fb3b62931e0ef4dc3797070fd1
Suprim Nakarmi <suprimnakarmi@gmail.com>
2024-11-09 20:30:00 -0600

    transfered multithreading code inside the heif_to_jpeg function

--- convert_to/heif2jpg.py
+++ convert_to/heif2jpg.py
@@ -14,14 +14,16 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 file_path= "/Users/suprimnakarmi/projects/ocr_reports/dataset/"
-all_file_names = glob.glob(file_path + "*.HEIC")
-start_time = time.perf_counter()
-register_heif_opener()
+dest_path = "/Users/suprimnakarmi/projects/ocr_reports/"
 
-def heif_to_jpeg(file):
+
+def conversion(file:str, dest_path:str):
     '''
-    Function to read file and convert it to jpeg format
-    file: Complete path of the file
+    converts the HEIC images to Jpeg
+
+    params
+        file: Complete path of the file (image)
+        dest_path: Complete path to saved the coverted images
     '''
     image_name = file.split("/")[-1]
     index_dot = image_name.find(".")
@@ -34,12 +36,24 @@ def heif_to_jpeg(file):
         raise IOError(f"An error occurred while trying to open the file '{file_name}.HEIC': {str(e)}")
     except Exception as e:
         raise RuntimeError(f"An unexpected error occurred while processing '{file_name}.HEIC: {str(e)}")
-    image.save(os.path.join("/Users/suprimnakarmi/projects/ocr_reports/",file_name + ".jpg"), format='jpeg')
+    image.save(os.path.join(dest_path,file_name + ".jpg"), format='jpeg')
 
 
-with ThreadPoolExecutor(max_workers=5) as executor:
-    executor.map(heif_to_jpeg,all_file_names)
+def heif_to_jpeg(file_path:str, dest_path:str):
+    '''
+    Use multithreading to write the jpeg file
 
+    params
+        file_path: complete path of the source files
+        dest_path: Complete path to saved the coverted images
+    '''
+    all_file_names = glob.glob(file_path + "*.HEIC")
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        executor.map(conversion,all_file_names,[dest_path] * len(all_file_names))
+
+start_time = time.perf_counter()
+register_heif_opener()
+heif_to_jpeg(file_path, dest_path)
 end_time = time.perf_counter()
 time_taken = end_time - start_time
 print(f"Time taken for conversion: {time_taken}")
@@ -52,5 +66,4 @@ Execution time (without multithreading): 24.43 seconds
 Execution time (with multithreading): 10.23 seconds
 
 Concurrent multithreading was used for its automatic allocation of threads to process. 
-
 '''
